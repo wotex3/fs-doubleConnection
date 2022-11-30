9c5a8de63415d1a206cbb3407236ee8f85f15756
whit333 <106171903+whit333@users.noreply.github.com>
2022-11-30 16:30:03 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,18 +5,18 @@
 
 Citizen.CreateThread(function()
 	while true do 
-		Citizen.Wait(7000)
-		if #GetPlayers() > 0 then 
-         doubleConnectDetection = {}
-			for k,v in pairs(GetPlayers()) do	
-				if IsPedAPlayer(GetPlayerPed(v)) ~= false then
-					if doubleConnectDetection[GetPlayerIdentifiers(v)[1]] == nil then 
-						doubleConnectDetection[GetPlayerIdentifiers(v)[1]] = 1
-					else 
-						DropPlayer(v, 'Double connect detection')
-					end 
-				end
-			end
-      end
+    Citizen.Wait(7000)
+    if #GetPlayers() > 0 then 
+     doubleConnectDetection = {}
+     for k,v in pairs(GetPlayers()) do	
+	if IsPedAPlayer(GetPlayerPed(v)) ~= false then
+	     if doubleConnectDetection[GetPlayerIdentifiers(v)[1]] == nil then 
+		   doubleConnectDetection[GetPlayerIdentifiers(v)[1]] = 1
+		else 
+		   DropPlayer(v, 'Double connect detection')
+		end 
+	   end
 	end
+      end
+   end
 end)
